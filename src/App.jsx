2b4f7522e80f95adfc0582c4ecc3f149bcbf08fb
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-11-28 00:16:59 +0530

    React router setup done and install tailwind and daisiui

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+   
+      <h1 className='text-4xl text-pink-500 text-center mt-6'>Fruit Store</h1>
+  
+    </>
+  )
+}
+
+export default App
